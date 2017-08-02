30d54deca1684b5de05b15ad00881b5532755127
Marissa Menking <marissamenking@Johns-MBP.lan>
2017-08-02 17:23:41 -0600

    Updating and Deleting Articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :articles, only: [:new, :create, :index, :show]
+  resources :articles, only: [:new, :create, :index, :show, :edit, :update, :delete, :destroy]
 end
--- spec/features/create_article_indices_spec.rb
+++ spec/features/create_article_indices_spec.rb
@@ -13,6 +13,27 @@ feature "Create Article Index", type: :feature do
     expect(page).to have_content article.body
 
     expect(page).to have_content article.title
+  end
+end
 
+feature "Update Articles", type: :feature do
+  scenario "Allows an article to be updated" do
+    article = Article.create title: 'Hello World', body: 'body content'
+
+    visit articles_path
+
+    expect(page).to have_content article.title
+
+    click_on "edit", match: :first
+  end
+end
+
+feature "Delete Articles", type: :feature do
+  scenario "Allows an article to be deleted" do
+    aritcle = Article.create title: 'Hello World', body: 'body content'
+
+    visit articles_path
+
+    click_on "delete"
   end
 end
